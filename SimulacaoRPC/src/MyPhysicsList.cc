9091b2d19b079487e19277d1c35dcc715eb8fdc3
VitorContatoPalaveri <143560984+VitorContatoPalaveri@users.noreply.github.com>
2025-10-27 15:44:16 -0300

    Update MyPhysicsList.cc

--- SimulacaoRPC/src/MyPhysicsList.cc
+++ SimulacaoRPC/src/MyPhysicsList.cc
@@ -1,9 +1,26 @@
 #include "MyPhysicsList.hh"
+#include <G4EmStandardPhysics.hh>
+#include <G4HadronPhysicsQGSP_BERT.hh>
+#include <G4DecayPhysics.hh>
+#include <G4IonElasticPhysics.hh>
 
-MyPhysicsList::MyPhysicsList(){
+MyPhysicsList::MyPhysicsList() {
+    //Inicializa os módulos de física
+    emPhysics = new G4EmStandardPhysics();             //Lista de física de interações eletromagnética padrão
+    hadronPhysics = new G4HadronPhysicsQGSP_BERT();    //Lista de física hadronica
+    decayPhysics = new G4DecayPhysics();               //Lista de física de decaimento
+    ionElasticPhysics = new G4IonElasticPhysics();     //Lista de física para simulação de interações de íons
 
-    // EM Physics
-    RegisterPhysics(new G4EmStandardPhysics());
+    //Registra os módulos de física
+    RegisterPhysics(emPhysics);
+    RegisterPhysics(hadronPhysics);
+    RegisterPhysics(decayPhysics);
+    RegisterPhysics(ionElasticPhysics);
 }
 
-MyPhysicsList::~MyPhysicsList(){}
+MyPhysicsList::~MyPhysicsList() {
+    delete emPhysics;
+    delete hadronPhysics;
+    delete decayPhysics;
+    delete ionElasticPhysics;
+}
